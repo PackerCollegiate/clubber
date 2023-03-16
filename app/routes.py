2ee0d2f56937bdf2fe5681a40a5a263d14a3551d
Mia Harlin <miharlin@packer.edu>
2023-03-15 21:58:28 -0400

    working on sprint 1
    Working on home page template. Won't actually render posts until sprint 2.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Mia'}
+    posts = [
+        {
+            'club': {'name': 'Math'},
+            'body': 'Come to Math Club!'
+        },
+        {
+            'club': {'name': 'Yearbook'},
+            'body': 'Reminder to take yearbook photos!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
