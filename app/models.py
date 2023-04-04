866a8b5f56dabe5eef55ece9686ece82633bbb46
Mia Harlin <miharlin@packer.edu>
2023-04-04 16:57:30 -0400

    debugging
    debugged issues with non existant columns and migration. also updated user and club relationship. not restricted to one to many anymore.

--- app/models.py
+++ app/models.py
@@ -7,7 +7,6 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    clubs = db.relationship('Club', backref='leader', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -27,7 +26,7 @@ class Club(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), index=True, unique=True)
     description = db.Column(db.String(300))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<{} Club>'.format(self.name)
