8750cc1dec344097d15651e63a09b610a8db70d5
Mia Harlin <miharlin@packer.edu>
2023-04-04 16:59:59 -0400

    variable rename in club table

--- app/models.py
+++ app/models.py
@@ -26,7 +26,7 @@ class Club(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40), index=True, unique=True)
     description = db.Column(db.String(300))
-    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<{} Club>'.format(self.name)
