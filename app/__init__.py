898814afdcf07f8b4cd7b3acf7b8507ad3b7c439
Mia Harlin <miharlin@packer.edu>
2023-04-29 18:00:18 -0400

    bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
